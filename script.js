dc65fc2c83217f6fc8fb6584c33979ea526462bc
indu74 <141615372+indu74@users.noreply.github.com>
2023-08-07 15:00:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,213 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3d1e2e7e9emsh896019cbfedbae7p1e544ajsn29bdff80880e',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+const getWeather = (city) => {
+      cityName.innerHTML = city
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+	.then(response => response.json())
+	.then((response) => {
+
+		console.log(response)
+
+		
+		temp.innerHTML = response.temp
+		temp6.innerHTML = response.temp
+		feels_like.innerHTML = response.feels_like
+		humidity.innerHTML = response.humidity
+		humidity6.innerHTML = response.humidity
+		min_temp.innerHTML = response.min_temp
+		max_temp.innerHTML = response.max_temp
+		wind_speed.innerHTML = response.wind_speed
+		wind_speed6.innerHTML = response.wind_speed
+		wind_degrees.innerHTML = response.wind_degrees
+		sunrise.innerHTML = response.sunrise
+		sunset.innerHTML = response.sunset
+	})
+
+	.catch(err => console.error(err));
+}
+
+submit.addEventListener("click", (e)=>{
+	e.preventDefault()
+	getWeather(city.value)
+})
+getWeather("Delhi")
+
+//function for weather of comman places
+
+
+const getWeather1 = (city1) => {
+	
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city1, options)
+  .then(response => response.json())
+  .then((response) => {
+
+	  console.log(response)
+
+	  
+	  temp1.innerHTML = response.temp
+	  feels_like1.innerHTML = response.feels_like
+	  humidity1.innerHTML = response.humidity
+	  min_temp1.innerHTML = response.min_temp
+	  max_temp1.innerHTML = response.max_temp
+	  wind_speed1.innerHTML = response.wind_speed
+	  wind_degrees1.innerHTML = response.wind_degrees
+	  sunrise1.innerHTML = response.sunrise
+	  sunset1.innerHTML = response.sunset
+  })
+
+  .catch(err => console.error(err));
+}
+
+
+  getWeather1("Hyderabad")
+
+//function2
+
+const getWeather2 = (city2) => {
+	
+
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city2, options)
+	  .then(response => response.json())
+	  .then((response) => {
+	
+		  console.log(response)
+	
+		  
+		  temp2.innerHTML = response.temp
+		  feels_like2.innerHTML = response.feels_like
+		  humidity2.innerHTML = response.humidity
+		  min_temp2.innerHTML = response.min_temp
+		  max_temp2.innerHTML = response.max_temp
+		  wind_speed2.innerHTML = response.wind_speed
+		  wind_degrees2.innerHTML = response.wind_degrees
+		  sunrise2.innerHTML = response.sunrise
+		  sunset2.innerHTML = response.sunset
+	  })
+	
+	  .catch(err => console.error(err));
+	}
+	
+	
+	  getWeather2("Pune")
+
+	  //function3
+
+	  const getWeather3 = (city3) => {
+	
+
+		fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city3, options)
+		  .then(response => response.json())
+		  .then((response) => {
+		
+			  console.log(response)
+		
+			  
+			  temp3.innerHTML = response.temp
+			  feels_like3.innerHTML = response.feels_like
+			  humidity3.innerHTML = response.humidity
+			  min_temp3.innerHTML = response.min_temp
+			  max_temp3.innerHTML = response.max_temp
+			  wind_speed3.innerHTML = response.wind_speed
+			  wind_degrees3.innerHTML = response.wind_degrees
+			  sunrise3.innerHTML = response.sunrise
+			  sunset3.innerHTML = response.sunset
+		  })
+		
+		  .catch(err => console.error(err));
+		}
+		
+		
+		  getWeather3("Mumbai")
+
+		  //function4
+
+		  const getWeather4 = (city4) => {
+	
+
+			fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city4, options)
+			  .then(response => response.json())
+			  .then((response) => {
+			
+				  console.log(response)
+			
+				  
+				  temp4.innerHTML = response.temp
+				  feels_like4.innerHTML = response.feels_like
+				  humidity4.innerHTML = response.humidity
+				  min_temp4.innerHTML = response.min_temp
+				  max_temp4.innerHTML = response.max_temp
+				  wind_speed4.innerHTML = response.wind_speed
+				  wind_degrees4.innerHTML = response.wind_degrees
+				  sunrise4.innerHTML = response.sunrise
+				  sunset4.innerHTML = response.sunset
+			  })
+			
+			  .catch(err => console.error(err));
+			}
+			
+			
+			  getWeather4("London")
+
+			  //function5
+
+			  const getWeather5 = (city5) => {
+	
+
+				fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city5, options)
+				  .then(response => response.json())
+				  .then((response) => {
+				
+					  console.log(response)
+				
+					  
+					  temp5.innerHTML = response.temp
+					  feels_like5.innerHTML = response.feels_like
+					  humidity5.innerHTML = response.humidity
+					  min_temp5.innerHTML = response.min_temp
+					  max_temp5.innerHTML = response.max_temp
+					  wind_speed5.innerHTML = response.wind_speed
+					  wind_degrees5.innerHTML = response.wind_degrees
+					  sunrise5.innerHTML = response.sunrise
+					  sunset5.innerHTML = response.sunset
+				  })
+				
+				  .catch(err => console.error(err));
+				}
+				
+				
+				  getWeather5("Bangkok")
+
+
+
+
+/*(async()=>{
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+	
+}
+
+catch (error) {
+	console.error(error);
+}})()
+
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3d1e2e7e9emsh896019cbfedbae7p1e544ajsn29bdff80880e',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi';*/
